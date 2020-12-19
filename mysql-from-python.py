d33e10c4d1237847adf404d3938950175493ebf3
TobinStonelake <tobinstonelake@outlook.com>
2020-12-19 14:16:32 +0000

    Completed Alternate Delete lesson

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -8,7 +8,7 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        rows = cursor.execute("DELETE FROM Friends WHERE name = 'Bob';")
+        rows = cursor.execute("DELETE FROM Friends WHERE name = %s;", 'Bob')
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
